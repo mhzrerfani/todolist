96e6860549f18fa99971e392e26ef1e73613d307
Mahziyar <mhzrerfani@gmail.com>
2022-07-11 15:38:20 +0430

    init commit 🔥

---
+++ apps/dnd-todolist/src/app/app.tsx
@@ -0,0 +1,16 @@
+export const App = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1 className="text-3xl font-bold text-pink-300">
+        Welcome to dnd-todolist!
+      </h1>
+      <img
+        width="450"
+        src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
+        alt="Nx - Smart, Fast and Extensible Build System"
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./apps/dnd-todolist/src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
